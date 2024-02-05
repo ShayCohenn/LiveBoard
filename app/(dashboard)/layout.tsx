a847ad3a3363fcc93205d68183b4fca105d7a189
ShayCohenn <shay91847@gmail.com>
2024-02-05 15:51:04 +0200

    cmt1

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,26 @@
+import Navbar from "./_components/navbar";
+import OrgSidebar from "./_components/org-sidebar";
+import { Sidebar } from "./_components/sidebar";
+
+type props = {
+  children: React.ReactNode;
+};
+
+const layout = (props: props) => {
+  return (
+    <main className="h-full">
+      <Sidebar />
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          <OrgSidebar />
+          <div className="h-full flex-1">
+            <Navbar />
+            {props.children}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default layout;
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://mint-monitor-58.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
