5a0e0f165c9947ad23e76a9f139ae7dd196722f6
ShayCohenn <shay91847@gmail.com>
2024-02-07 15:40:22 +0200

    added board schema, create mutation and 10 svgs

---
+++ convex/board.ts
@@ -0,0 +1,33 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+const images: string[] = [];
+
+for (let i = 1; i <= 10; i++) {
+  const imagePath = `/placeholders/${i}.svg`;
+  images.push(imagePath);
+}
+
+export const create = mutation({
+  args: {
+    orgId: v.string(),
+    title: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+
+    const board = await ctx.db.insert("boards", {
+      title: args.title,
+      orgId: args.orgId,
+      authorId: identity.subject,
+      authorName: identity.name!,
+      imageUrl: randomImage,
+    });
+    return board;
+  },
+});
