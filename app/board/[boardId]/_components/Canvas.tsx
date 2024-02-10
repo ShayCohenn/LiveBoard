78c273a6adec552c5a1816b8f83f7173f458aaa1
ShayCohenn <shay9847@gmail.com>
2024-02-10 12:12:05 +0200

    live room authentication

--- app/board/[boardId]/_components/Canvas.tsx
+++ app/board/[boardId]/_components/Canvas.tsx
@@ -3,12 +3,16 @@
 import Info from "./Info";
 import Participants from "./Participants";
 import Toolbar from "./Toolbar";
+import { useSelf } from "@/liveblocks.config";
 
 type props = {
   boardId: string;
 };
 
 const Canvas = (props: props) => {
+  const info = useSelf((me) => me.info)
+  console.log(info);
+  
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info />
