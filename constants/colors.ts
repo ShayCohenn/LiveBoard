2b1a65b4956ba73f24df16461bddfcce1a84ea07
ShayCohenn <shay91847@gmail.com>
2024-05-20 15:59:52 +0300

    colors

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,9 +1,16 @@
 import { Color } from "@/types/canvas";
 
 export const COLORS: Color[] = [
-    {r: 20, g: 20, b: 20},
-    {r: 20, g: 20, b: 240},
-    {r: 20, g: 240, b: 20},
-    {r: 240, g: 20, b: 20},
-    {r: 255, g: 255, b: 255}
-]
+    { r: 20, g: 20, b: 20 },     // Dark gray
+    { r: 20, g: 20, b: 240 },    // Blue
+    { r: 20, g: 240, b: 20 },    // Green
+    { r: 240, g: 20, b: 20 },    // Red
+    { r: 255, g: 255, b: 255 },  // White
+    { r: 240, g: 240, b: 20 },   // Yellow
+    { r: 240, g: 20, b: 240 },   // Magenta
+    { r: 20, g: 240, b: 240 },   // Cyan
+    { r: 128, g: 0, b: 128 },    // Purple
+    { r: 255, g: 165, b: 0 },    // Orange
+    { r: 0, g: 128, b: 128 },    // Teal
+    { r: 255, g: 0, b: 255 }     // Pink
+];
